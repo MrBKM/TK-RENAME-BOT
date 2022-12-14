7745655fcf7946a916650dc60c0fef9963e096f3
BKM TG <119480777+MrBKM@users.noreply.github.com>
2022-12-14 21:26:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "25085908")
+API_ID = os.environ.get("API_ID", "12694006")
 
-API_HASH = os.environ.get("API_HASH", "86b8a77b1ecb0507a3da3934e6a2c184")
+API_HASH = os.environ.get("API_HASH", "8719d11809492f836004a39b42599215")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5731006395:AAE9_ivch-AaObbG6EJD0gaEjXQJftN5ikE") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5959839520:AAGZzJDyN5u4gxj4g5ma3aiFQ3nWY6H-0XA") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1001515447343") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1001758428532") 
 
 DB_NAME = os.environ.get("DB_NAME","Balmiki")     
 
@@ -16,7 +16,7 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://Balmiki:Balmiki@cluster0.lmden.m
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/11d6ae7240375c78d2782.jpg")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/343f4d2225312f099f448.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5702578360').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '953377581').split()]
 
